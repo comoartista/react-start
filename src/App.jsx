c9a2077dab5c0e15b405b3f7f4c5a5b765c0e378
Oksana Melnyk <oks.melnuk@gmail.com>
2024-01-29 20:27:45 +0100

    basic app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 
-
 function App() {
-
-  return <p>Very cool</p>
+  return (
+    <main>
+      <div>
+        <h1>Fancy Counter</h1>
+        <p>0</p>
+        <button>Reset</button>
+        <div>
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
